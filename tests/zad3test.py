99c6ab1a4fc7e2818761e4046cad69d7d37fc95a
Sienkowski <p.sienkowski@onet.pl>
2020-11-03 23:29:47 +0100

    zad3 green test4

--- tests/zad3test.py
+++ tests/zad3test.py
@@ -15,7 +15,7 @@ class ChristmasSongTest(unittest.TestCase):
     def test_2_verse(self):
         self.assertEqual(self.xMasSong.singOneLine(1), 'On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree.')
 
-    @unittest.skip("not implemented")
+    # @unittest.skip("not implemented")
     def test_3_verse(self):
         self.assertEqual(self.xMasSong.singOneLine(2), 'On the third day of Christmas my true love gave to me: three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.')
 
