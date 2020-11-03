11985ba62122df9d33b666ec20027383be75b790
Sienkowski <p.sienkowski@onet.pl>
2020-11-03 23:28:18 +0100

    zad3 red test1

--- tests/zad3test.py
+++ tests/zad3test.py
@@ -3,53 +3,67 @@ from src.zad3 import Song
 
 class ChristmasSongTest(unittest.TestCase):
 
+    # @unittest.skip("not implemented")
     def test_whole_song(self):
         self.assertEqual(self.xMasSong.singWholeSong(), ['On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.', 'On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree.', 'On the third day of Christmas my true love gave to me: three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.', 'On the fourth day of Christmas my true love gave to me: four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.', 'On the fifth day of Christmas my true love gave to me: five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.', 'On the sixth day of Christmas my true love gave to me: six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.', 'On the seventh day of Christmas my true love gave to me: seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.', 'On the eighth day of Christmas my true love gave to me: eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.', 'On the ninth day of Christmas my true love gave to me: nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.', 'On the tenth day of Christmas my true love gave to me: ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.', 'On the eleventh day of Christmas my true love gave to me: eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.', 'On the twelfth day of Christmas my true love gave to me: twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.'])
 
+    @unittest.skip("not implemented")
     def test_1_verse(self):
         self.assertEqual(self.xMasSong.singOneLine(0), 'On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.')
 
+    @unittest.skip("not implemented")
     def test_2_verse(self):
         self.assertEqual(self.xMasSong.singOneLine(1), 'On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree.')
 
+    @unittest.skip("not implemented")
     def test_3_verse(self):
         self.assertEqual(self.xMasSong.singOneLine(2), 'On the third day of Christmas my true love gave to me: three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.')
 
+    @unittest.skip("not implemented")
     def test_4_verse(self):
-        self.assertEqual(self.xMasSong.singOneLine(3), 'On the fourth day of Christmas my true love gave to me: four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.')
+        self.assertEqual(self.xMasSong.singOneLine(3), 'On the fourth day of Christmas my true love gave to me: four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.')\
 
+    @unittest.skip("not implemented")
     def test_5_verse(self):
         self.assertEqual(self.xMasSong.singOneLine(4), 'On the fifth day of Christmas my true love gave to me: five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.')
 
+    @unittest.skip("not implemented")
     def test_multiple_6_to_9(self):
         self.assertEqual(self.xMasSong.singMultipleLines(5, 8), ['On the sixth day of Christmas my true love gave to me: six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.', 'On the seventh day of Christmas my true love gave to me: seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.', 'On the eighth day of Christmas my true love gave to me: eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.', 'On the ninth day of Christmas my true love gave to me: nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.'])
 
+    @unittest.skip("not implemented")
     def test_mutiple_10_to_12(self):
         self.assertEqual(self.xMasSong.singMultipleLines(10, 12), ['On the tenth day of Christmas my true love gave to me: ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.', 'On the eleventh day of Christmas my true love gave to me: eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.', 'On the twelfth day of Christmas my true love gave to me: twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.'])
 
+    @unittest.skip("not implemented")
     def test_multiple_1_to_1(self):
         self.assertEqual(self.xMasSong.singMultipleLines(1, 1), ['On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.'])
 
+    @unittest.skip("not implemented")
     def test_larger_than_12(self):
         with self.assertRaisesWithMessage(ValueError):
             self.xMasSong.singOneLine(15)
 
+    @unittest.skip("not implemented")
     def test_range_over_12(self):
         with self.assertRaisesWithMessage(ValueError):
             self.xMasSong.singMultipleLines(15, 20)
 
+    @unittest.skip("not implemented")
     def test_negative_val(self):
         with self.assertRaisesWithMessage(ValueError):
             self.xMasSong.singOneLine(-12)
 
+    @unittest.skip("not implemented")
     def test_string_not_num(self):
         with self.assertRaisesWithMessage(ValueError):
             self.xMasSong.singOneLine("1")
 
+    @unittest.skip("not implemented")
     def test_range_string_not_num(self):
         with self.assertRaisesWithMessage(ValueError):
             self.xMasSong.singMultipleLines("1", 3)
-            
+
     # Utility functions
     def setUp(self):
         self.xMasSong = Song()
