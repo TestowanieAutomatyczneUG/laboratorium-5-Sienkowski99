3dcc2976127a88380d39275f42b160144e0b0846
Sienkowski <p.sienkowski@onet.pl>
2020-11-03 21:09:48 +0100

    zad1 refactor test5

--- src/zad1.py
+++ src/zad1.py
@@ -1,11 +1,5 @@
 class Hamming:
     def distance(self, a, b):
-        if a == "" and b == "":
-            return 0
-        if a == b:
-            return 0
-        # if a != b:
-        #     return 1
         arrA = []
         arrB = []
         for letter in a:
