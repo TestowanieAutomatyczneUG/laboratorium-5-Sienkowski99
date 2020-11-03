cfa17d7b4e7f931a486d160d2578d384cde7fb08
Sienkowski <p.sienkowski@onet.pl>
2020-11-03 22:16:51 +0100

    zad2 red test13

--- src/zad2.py
+++ src/zad2.py
@@ -1,20 +1,23 @@
 def roman(num):
     symbols = {
         1: "I",
-        2: "II",
-        3: "III",
         4: "IV",
         5: "V",
-        6: "VI",
-        7: "VII",
-        8: "VIII",
         9: "IX",
         10: "X",
+        40: "XV",
         50: "L",
+        90: "XC",
         100: "C",
+        400: "CD",
         500: "D",
+        900: "CM",
         1000: "M",
     }
+    helper = []
+    for key in symbols:
+        helper.append(key)
+    print(helper)
     result = ""
     if num in symbols:
         result = symbols[num]
