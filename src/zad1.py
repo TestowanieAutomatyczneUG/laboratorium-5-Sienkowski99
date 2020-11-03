175b0a239cede5ee277553ed22d218d1f0cf071e
Sienkowski <p.sienkowski@onet.pl>
2020-11-03 21:21:20 +0100

    zad1 refactor test8

--- src/zad1.py
+++ src/zad1.py
@@ -2,16 +2,10 @@ class Hamming:
     def distance(self, a, b):
         if len(a) != len(b):
             raise ValueError('err')
-        arrA = []
-        arrB = []
-        for letter in a:
-            arrA.append(letter)
-        for letter in b:
-            arrB.append(letter)
         result = 0
-        for i in range(0, len(arrA)):
-            if arrA[i] != arrB[i]:
-                result +=1
+        for i in range(0, len(a)):
+            if a[i] != b[i]:
+                result += 1
             else:
                 pass
         return result
