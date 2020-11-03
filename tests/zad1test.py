211235fb9d862d7a5b482ea89e57fe5c38146045
Sienkowski <p.sienkowski@onet.pl>
2020-11-03 21:00:25 +0100

    zad1 red test3

--- tests/zad1test.py
+++ tests/zad1test.py
@@ -13,7 +13,7 @@ class HammingTest(unittest.TestCase):
     def test_single_letter_identical_strands(self):
         self.assertEqual(hamming.distance("A", "A"), 0)
 
-    @unittest.skip("not implemented")
+    # @unittest.skip("not implemented")
     def test_single_letter_different_strands(self):
         self.assertEqual(hamming.distance("G", "T"), 1)
 
