2845546a5a9b38fcf8259fb0499ca154eb9a831c
Sienkowski <p.sienkowski@onet.pl>
2020-11-03 21:09:18 +0100

    zad1 green test5

--- src/zad1.py
+++ src/zad1.py
@@ -4,5 +4,18 @@ class Hamming:
             return 0
         if a == b:
             return 0
-        if a != b:
-            return 1
+        # if a != b:
+        #     return 1
+        arrA = []
+        arrB = []
+        for letter in a:
+            arrA.append(letter)
+        for letter in b:
+            arrB.append(letter)
+        result = 0
+        for i in range(0, len(arrA)):
+            if arrA[i] != arrB[i]:
+                result +=1
+            else:
+                pass
+        return result
