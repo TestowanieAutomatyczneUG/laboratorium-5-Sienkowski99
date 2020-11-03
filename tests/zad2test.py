3571988990e954b3ff61ae3d7b18f93a8119ef32
Sienkowski <p.sienkowski@onet.pl>
2020-11-03 21:56:36 +0100

    zad2 red test9

--- tests/zad2test.py
+++ tests/zad2test.py
@@ -34,7 +34,7 @@ class RomanNumeralsTest(unittest.TestCase):
     def test_20_is_two_x_s(self):
         self.assertEqual(roman(27), "XXVII")
 
-    @unittest.skip("not implemented")
+    # @unittest.skip("not implemented")
     def test_48_is_not_50_2_but_rather_40_8(self):
         self.assertEqual(roman(48), "XLVIII")
 
