2cbd6e995170e2a811532342ec15a13670e46e62
Sienkowski <p.sienkowski@onet.pl>
2020-11-03 21:56:13 +0100

    zad2 green test8

--- src/zad2.py
+++ src/zad2.py
@@ -9,7 +9,15 @@ def roman(num):
         7: "VII",
         8: "VIII",
         9: "IX",
-        10: "X"
+        10: "X",
+        50: "L",
+        100: "C",
+        500: "D",
+        1000: "M",
     }
+    result = ""
     if num <= 10:
         return symbols[num]
+    if num <40:
+        result += "X" * (num // 10) + symbols[num % 10]
+    return result
